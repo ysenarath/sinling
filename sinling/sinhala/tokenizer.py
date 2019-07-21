5db8817fb5bd5b5ceb7c49dbca56c1096779256a
Yasas <wayasas@gmail.com>
2019-07-21 13:10:34 +0530

    Add sinhala tweet tokenizer.

--- sinling/sinhala/tokenizer.py
+++ sinling/sinhala/tokenizer.py
@@ -1,11 +1,17 @@
 import re
+from typing import Tuple, Text, Dict, List
+
+import emoji
+
+Boolean = bool
 
 __all__ = [
-    'SinhalaTokenizer'
+    'SinhalaTokenizer',
+    'SinhalaTweetTokenizer'
 ]
 
 
-def is_a_sinhala_letter(s: str) -> bool:
+def is_a_sinhala_letter(s: Text) -> Boolean:
     if len(s) != 1:
         return True
     sinhala_lower_bound = 3456
@@ -16,224 +22,109 @@ def is_a_sinhala_letter(s: str) -> bool:
     return False
 
 
-def contains_sinhala(s: str) -> bool:
+def contains_sinhala(s: Text) -> Boolean:
     for c in s:
         if is_a_sinhala_letter(c):
             return True
     return False
 
 
+# noinspection SpellCheckingInspection
 class Tokenizer:
-    def tokenize(self, sentence):
+    def tokenize(self, sentence: Text) -> List[Text]:
         raise NotImplementedError()
 
 
+# noinspection SpellCheckingInspection
 class SinhalaTokenizer(Tokenizer):
     def __init__(self):
-        self.ignoring_chars: list = []
-        self.isolate_punctuations_with_spaces: bool = False
-        self.punctuation_marks: list = [
-            ".", ",", "\n", " ", "¸", "‚",
-            "\"", "/", "-", "|", "\\", "—", "¦",
-            "”", "‘", "'", "“", "’", "´", "´",
-            "!", "@", "#", "$", "%", "^", "&", "\\*", "+", "\\-", "£", "\\?", "˜",
-            "\\(", "\\)", "\\[", "\\]", "{", "}",
-            ":", ";",
-            "\u2013"  # EN - DASH
+        self.isolate_punctuations_with_spaces: Boolean = False
+        self.punctuation_marks: List[Text] = [
+            '.', ',', '\n', ' ', '¸', '‚',
+            '"', '/', '-', '|', '\\', '—', '¦',
+            '”', '‘', '\'', '“', '’', '´', '´',
+            '!', '@', '#', '$', '%', '^', '&', '*', '+', '-', '£', '?', '˜',
+            '(', ')', '[', ']', '{', '}',
+            ':', ';',
+            '\u2013'  # EN - DASH
         ]
-        self.invalid_chars: list = [
-            "Ê",
-            "\u00a0", "\u2003",  # spaces
-            "\ufffd", "\uf020", "\uf073", "\uf06c", "\uf190",  # unknown or invalid unicode chars
-            "\u202a", "\u202c", "\u200f"  # direction control chars(for arabic, starting from right etc)
+        self.invalid_chars: List[Text] = [
+            'Ê',
+            '\u00a0', '\u2003',  # spaces
+            '\ufffd', '\uf020', '\uf073', '\uf06c', '\uf190',  # unknown or invalid unicode chars
+            '\u202a', '\u202c', '\u200f'  # direction control chars(for arabic, starting from right etc)
         ]
-        self.word_tokenizer_delims: str = ""
-        self.line_tokenizing_chars: list = [
-            ".", "?", "!", ":", ";", "\u2022"
+        self.line_tokenizing_chars: List[Text] = [
+            '.', '?', '!', ':', ';', '\u2022'
         ]
-        self.punctuations_without_line_tokenizing_chars: list = [
-            ",", "¸", "‚",
-            "\"", "/", "-", "\\|", "\\\\", "—", "¦",
-            "”", "‘", "'", "“", "’", "´", "´",
-            "!", "@", "#", "\\$", "\\%", "\\^", "\\&",
-            "\\*", "\\+", "\\-", "£", "\\?", "˜",
-            "\\(", "\\)", "\\[", "\\]", "\\{", "\\}",
-            ":", ";",
-            "\u2013"
+        self.punctuations_without_line_tokenizing_chars: List[Text] = [
+            ',', '¸', '‚',
+            '"', '/', '-', '|', '\\', '—', '¦',
+            '”', '‘', '\'', '“', '’', '´', '´',
+            '!', '@', '#', '$', '%', '^', '&',
+            '*', '+', '-', '£', '?', '˜',
+            '(', ')', '[', ']', '{', '}',
+            ':', ';',
+            '\u2013'
         ]
-        self.short_forms: list = [
-            "ඒ.", "බී.", "සී.", "ඩී.", "ඊ.", "එෆ්.", "ජී.", "එච්.",
-            "අයි.", "ජේ.", "කේ.", "එල්.", "එම්.", "එන්.", "ඕ.",
-            "පී.", "කිව්.", "ආර්.", "එස්.", "ටී.", "ය.", "ඩබ.", "ඩබ්ලිව්.",
-            "එක්ස්.", "වයි.", "ඉසෙඩ්.",
-            "පෙ.", "ව.",
-            "රු.",
-            "පා.",  # parliment
-            "0.", "1.", "2.", "3.", "4.", "5.", "6.", "7.", "8.", "9."
+        self.short_forms: List[Text] = [
+            'ඒ.', 'බී.', 'සී.', 'ඩී.', 'ඊ.', 'එෆ්.', 'ජී.', 'එච්.',
+            'අයි.', 'ජේ.', 'කේ.', 'එල්.', 'එම්.', 'එන්.', 'ඕ.',
+            'පී.', 'කිව්.', 'ආර්.', 'එස්.', 'ටී.', 'ය.', 'ඩබ.', 'ඩබ්ලිව්.',
+            'එක්ස්.', 'වයි.', 'ඉසෙඩ්.',
+            'පෙ.', 'ව.',
+            'රු.',
+            'පා.',  # parliment
+            '0.', '1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.'
         ]
-        self.short_form_identifier: str = "\u0D80"
-        self.line_tokenizer_delims: str = ""
 
-        for s in self.punctuations_without_line_tokenizing_chars:
-            if self.short_form_identifier == s:
-                print("Do not use " + self.short_form_identifier + " at punctuation list.")
+        # Do not use `short_form_identifier` at `punctuation_marks`
+        self.short_form_identifier: Text = '\u0D80'
 
         #  init ignoring chars
-        self._init_ignoring_chars()
+        self.ignoring_chars: List[Text] = [
+            '\u200c', '\u0160', '\u00ad', '\u0088', '\uf086', '\u200b', '\ufeff', 'Á', 'À', '®', '¡', 'ª', 'º', '¤',
+            '¼', '¾', 'Ó', 'ø', '½', 'ˆ', '', '¢', 'ÿ', '·', 'í', 'Ω', '°', '×', 'µ', '', '~', 'ƒ', '', 'ë', 'Î',
+            '‰', '»', '«', 'à', '«', '·', '¨', '…', '⋆', '›', '¥', '⋆', '', '˝', '', '', '◊', 'Ł', '', 'ê', 'Õ', 'Ä',
+            'á', 'Ñ', 'Í', '', 'Ñ', 'ç', 'Æ', 'ô', 'Ž', '€', '§', 'Æ', '÷', 'é', '¯', 'é', 'æ', 'î', 'ï', 'ä', 'Ô', 'õ',
+            'È', 'Ý', 'ß', 'õ', '', 'ù', 'å', 'Ø', 'Œ', 'Ô', 'Ü', '', 'Ö', 'Û', 'Ï', 'ñ', 'ý', 'œ', '¹', '', 'É', '¯',
+            'Ò', '\u200d'
+        ]
 
         # init word tokenizer
-        tmp = "["
-        for s in self.punctuation_marks:
-            tmp += s
-        for s in self.invalid_chars:
-            tmp += s
-        tmp += "]"
-        self.word_tokenizer_delims = tmp
+        self.word_tokenizer_delims: Text = '[{}]'.format(
+            re.escape(''.join(self.punctuation_marks + self.invalid_chars)))
 
         # init line tokenizer
-        tmp = "["
-        for s in self.line_tokenizing_chars:
-            tmp += s
-        tmp += "]"
-        self.line_tokenizer_delims = tmp
+        self.line_tokenizer_delims: Text = '[{}]'.format(re.escape(''.join(self.line_tokenizing_chars)))
 
-    def set_isolate_punctuations_with_spaces(self, state: bool):
-        self.isolate_punctuations_with_spaces = state
-
-    def _init_ignoring_chars(self):
-        self.ignoring_chars.append("\u200c")
-        self.ignoring_chars.append("\u0160")
-        self.ignoring_chars.append("\u00ad")
-        self.ignoring_chars.append("\u0088")
-        self.ignoring_chars.append("\uf086")
-        self.ignoring_chars.append("\u200b")
-        self.ignoring_chars.append("\ufeff")
-        self.ignoring_chars.append("Á")
-        self.ignoring_chars.append("À")
-        self.ignoring_chars.append("®")
-        self.ignoring_chars.append("¡")
-        self.ignoring_chars.append("ª")
-        self.ignoring_chars.append("º")
-        self.ignoring_chars.append("¤")
-        self.ignoring_chars.append("¼")
-        self.ignoring_chars.append("¾")
-        self.ignoring_chars.append("Ó")
-        self.ignoring_chars.append("ø")
-        self.ignoring_chars.append("½")
-        self.ignoring_chars.append("ˆ")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("¢")
-        self.ignoring_chars.append("ÿ")
-        self.ignoring_chars.append("·")
-        self.ignoring_chars.append("í")
-        self.ignoring_chars.append("Ω")
-        self.ignoring_chars.append("°")
-        self.ignoring_chars.append("×")
-        self.ignoring_chars.append("µ")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("~")
-        self.ignoring_chars.append("ƒ")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("ë")
-        self.ignoring_chars.append("Î")
-        self.ignoring_chars.append("‰")
-        self.ignoring_chars.append("»")
-        self.ignoring_chars.append("«")
-        self.ignoring_chars.append("à")
-        self.ignoring_chars.append("«")
-        self.ignoring_chars.append("·")
-        self.ignoring_chars.append("¨")
-        self.ignoring_chars.append("…")
-        self.ignoring_chars.append("⋆")
-        self.ignoring_chars.append("›")
-        self.ignoring_chars.append("¥")
-        self.ignoring_chars.append("⋆")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("˝")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("◊")
-        self.ignoring_chars.append("Ł")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("ê")
-        self.ignoring_chars.append("Õ")
-        self.ignoring_chars.append("Ä")
-        self.ignoring_chars.append("á")
-        self.ignoring_chars.append("Ñ")
-        self.ignoring_chars.append("Í")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("Ñ")
-        self.ignoring_chars.append("ç")
-        self.ignoring_chars.append("Æ")
-        self.ignoring_chars.append("ô")
-        self.ignoring_chars.append("Ž")
-        self.ignoring_chars.append("€")
-        self.ignoring_chars.append("§")
-        self.ignoring_chars.append("Æ")
-        self.ignoring_chars.append("÷")
-        self.ignoring_chars.append("é")
-        self.ignoring_chars.append("¯")
-        self.ignoring_chars.append("é")
-        self.ignoring_chars.append("æ")
-        self.ignoring_chars.append("î")
-        self.ignoring_chars.append("ï")
-        self.ignoring_chars.append("ä")
-        self.ignoring_chars.append("Ô")
-        self.ignoring_chars.append("õ")
-        self.ignoring_chars.append("È")
-        self.ignoring_chars.append("Ý")
-        self.ignoring_chars.append("ß")
-        self.ignoring_chars.append("õ")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("ù")
-        self.ignoring_chars.append("å")
-        self.ignoring_chars.append("Ø")
-        self.ignoring_chars.append("Œ")
-        self.ignoring_chars.append("Ô")
-        self.ignoring_chars.append("Ü")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("Ö")
-        self.ignoring_chars.append("Û")
-        self.ignoring_chars.append("Ï")
-        self.ignoring_chars.append("ñ")
-        self.ignoring_chars.append("ý")
-        self.ignoring_chars.append("œ")
-        self.ignoring_chars.append("¹")
-        self.ignoring_chars.append("")
-        self.ignoring_chars.append("É")
-        self.ignoring_chars.append("¯")
-        self.ignoring_chars.append("Ò")
-
-    def tokenize(self, s: str) -> list:
+    def tokenize(self, sentence: Text) -> List[Text]:
         # remove ignoring chars from document
         for ignoring_char in self.ignoring_chars:
-            if ignoring_char in s:
-                s = s.replace(ignoring_char, "")
+            if ignoring_char in sentence:
+                sentence = sentence.replace(ignoring_char, '')
 
         # prevent short forms being splitted into separate tokens
         # Eg: පෙ.ව.
         for short_form in self.short_forms:
-            representation = short_form[0:len(short_form) - 1] + self.short_form_identifier
-            s = s.replace(short_form, representation)
+            representation = short_form[0:-1] + self.short_form_identifier
+            sentence = sentence.replace(short_form, representation)
 
-        parts = re.split(r'({})'.format(self.word_tokenizer_delims), s)
-        tokens = []
-        for token in parts:
-            token = token.replace(self.short_form_identifier, ".")
-            tokens.append(token)
+        parts = re.split(r'({})'.format(self.word_tokenizer_delims), sentence)
+        tokens = [token.replace(self.short_form_identifier, '.') for token in parts if len(token.strip()) != 0]
         return tokens
 
-    def split_sentences(self, doc: str) -> list:
+    def split_sentences(self, doc: Text, return_sinhala_only: Boolean = False) -> List[Text]:
         # remove ignoring chars from document
         for ignoring_char in self.ignoring_chars:
             if ignoring_char in doc:
-                doc = doc.replace(ignoring_char, "")
+                doc = doc.replace(ignoring_char, '')
 
         # stop words being present with a punctuation at start or end of the word
         # Eg: word?     word,
         if self.isolate_punctuations_with_spaces:  # default is set to FALSE
             for punctuation in self.punctuations_without_line_tokenizing_chars:
-                doc = doc.replace(punctuation, " " + punctuation + " ")
+                doc = doc.replace(punctuation, ' ' + punctuation + ' ')
 
         # prevent short forms being splitted into sentences
         # Eg: පෙ.ව.
@@ -243,10 +134,68 @@ class SinhalaTokenizer(Tokenizer):
 
         sentences = []
         # split lines
-        parts = re.split(r"{}".format(self.line_tokenizer_delims), doc)
+        parts = re.split(r'{}'.format(self.line_tokenizer_delims), doc)
         for sentence in parts:
-            sentence = sentence.replace(self.short_form_identifier, ".")
+            sentence = sentence.replace(self.short_form_identifier, '.')
             sentence = sentence.strip()
             if contains_sinhala(sentence):  # filter empty sentences and non-sinhala sentences
                 sentences.append(sentence)
+            elif not return_sinhala_only and len(sentence) != 0:
+                sentences.append(sentence)
+        return sentences
+
+
+# noinspection SpellCheckingInspection
+class SinhalaTweetTokenizer(Tokenizer):
+    def __init__(self):
+        self.tokenizer = SinhalaTokenizer()
+        self._special_chars = ['_']
+        self._special_chars_map = str.maketrans({ord(c): '_{}'.format(c) for c in self._special_chars})
+        self._var_type_pattern = {
+            'hashtag': r'#\w+',
+            'mention': r'@\w+',
+            'url': r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+        }  # for creation of lookups
+
+    def escape(self, string: Text) -> Tuple[Text, Dict[Text, Tuple[Text, Text]]]:
+        """
+        Escape special characters in a string.
+        """
+        lookup = {}
+        string = string.translate(self._special_chars_map)
+        var_id: int = 0
+        for var_type, pattern in self._var_type_pattern.items():
+            vals = re.findall(pattern, string)
+            for v in vals:
+                var, val = 'VAR_{}'.format(var_id), v
+                lookup[var] = (val, var_type)
+                string = string.replace(val, var)
+                var_id += 1
+        return string, lookup
+
+    # noinspection PyMethodMayBeStatic
+    def unescape(self, string: Text, lookup: Dict[Text, Tuple[Text, Text]]) -> Text:
+        """
+        UnEscape special characters in a string.
+        """
+        for var, val in lookup.items():
+            string = string.replace(var, val[0])
+        return re.sub(r'_(.)', r'\1', string)
+
+    def tokenize(self, sentence: Text) -> List[Text]:
+        """
+        Tokenize the input sentence(tweet) and return `List[Text]` containing tokens.
+        """
+        sentence, lookup = self.escape(sentence)
+        for e in emoji.UNICODE_EMOJI:
+            if e in sentence:
+                sentence = sentence.replace(e, ' {} '.format(e))
+        sentence = re.sub(r'\xa0', ' ', sentence)
+        sentence = re.sub(r' +', ' ', sentence)
+        tokens = [self.unescape(token, lookup) for token in self.tokenizer.tokenize(sentence)]
+        return tokens
+
+    def split_sentences(self, doc: Text, return_sinhala_only: Boolean = False) -> List[Text]:
+        doc, lookup = self.escape(doc)
+        sentences = [self.unescape(token, lookup) for token in self.tokenizer.split_sentences(doc, return_sinhala_only)]
         return sentences
