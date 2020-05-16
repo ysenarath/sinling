c057fbce6ac8d91d3983b367e1e3d85cc739272a
sajithdherath <sajithdherath@gmail.com>
2020-05-16 16:16:48 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=['emoji'],
