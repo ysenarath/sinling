193d5da09f531e2b0ae88a19db9618ace983699d
Yasas Senarath <ysenarath.93@gmail.com>
2020-11-01 01:04:17 -0400

    Update package version.   Include resources in package.

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
+
+with open('packages.txt', 'r') as fh:
+    requirements = fh.read().split('\n')
 
 setuptools.setup(
     name="sinling",  # Replace with your own username
-    version="v0.2.0",
+    version="0.3.0",
     author="Yasas Senarath",
     description="A language processing tool for Sinhalese (සිංහල)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ysenarath/sinling",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    include_package_data=True,
 )
